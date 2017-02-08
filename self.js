5d1fb1f0b66bbc85bfa509f02d111c2470a6871f
TheRacingLion <correiasebastiao307@gmail.com>
2017-02-08 13:28:56 +0000

    Making streamed game rotation an option

--- self.js
+++ self.js
@@ -67,16 +67,12 @@ fs.readdir(path.join(__dirname, 'commands/'), (err, files) => {
 self.on('ready', () => {
   log.ready(self, config)
   if (config.rotatePlayingGame && games.length > 0) {
-    log.log('Changing playing game every ' + (config.rotatePlayingGameTime / 1000) / 60 + ' minutes.', 'Config')
+    const stream = config.rotatePlayingGameInStreamingStatus
+    log.log(`Changing playing game ${stream ? 'in streaming status ' : ''}every ` + (config.rotatePlayingGameTime / 1000) / 60 + ' minutes.', 'Config')
     setInterval(() => {
-      self.editStatus(config.defaultStatus.toLowerCase(), {name: games[~~(Math.random() * games.length)]})
+      const game = games[~~(Math.random() * games.length)]
+      self.editStatus(config.defaultStatus.toLowerCase(), stream ? {name: game, type: 1, url: 'https://www.twitch.tv/twitch'} : {name: game})
     }, config.rotatePlayingGameTime) // Edits playing game every X milliseconds (You can edit this number in the config file)
-  }
-  if (config.rotateStreamingGame && streams.length > 0) {
-    log.log('Changing streaming status every ' + (config.rotateStreamingStatusTime / 1000) / 60 + ' minutes.', 'Config')
-    setInterval(() => {
-      self.editStatus(config.defaultStatus.toLowerCase(), {name: games[~~(Math.random() * games.length)], type: 1, url: 'https://www.twitch.tv/twitch'})
-    }, config.rotateStreamingStatusTime) // Edits streaming status every X milliseconds (You can edit this number in the config file)
   }
   if (config.rotateAvatarImage && avatars.length > 0) {
     log.log('Changing avatar every ' + (config.rotateAvatarImageTime / 1000) / 60 + ' minutes.', 'Config')
