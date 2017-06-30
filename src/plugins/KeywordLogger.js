7fe26728a6d13edf16a7bbaa295939ea3839621e
TheRacingLion <correiasebastiao307@gmail.com>
2017-06-30 17:02:40 +0100

    :bug: | Change the way webhooks are created

--- src/plugins/KeywordLogger.js
+++ src/plugins/KeywordLogger.js
@@ -23,30 +23,38 @@ module.exports = (self, log, config) => {
           if (config.keywordNotificator.inNotificationChannel) {
             if (Object.keys(self.channelGuildMap).includes(config.notificationChannelID)) {
               self.getMessages(msg.channel.id, 4, msg.id).then(msgs => {
-                self.createChannelWebhook(config.notificationChannelID, { name: msg.author.username }).then(w => {
-                  self.counts.msgsSent = self.counts.msgsSent + 1
-                  self.executeWebhook(w.id, w.token, {
-                    username: msg.author.username,
-                    avatarURL: msg.author.avatarURL,
-                    content: `**I mentioned keyword \`${keywords[i]}\` in** ${msg.channel.mention}!`,
-                    embeds: [{
-                      title: moment(msg.timestamp).format('ddd, Do of MMM @ HH:mm:ss'),
-                      author: {
-                        name: `${msg.author.username}#${msg.author.discriminator} (${msg.author.id})`,
-                        icon_url: msg.author.avatarURL
-                      },
-                      color: 16426522,
-                      fields: [
-                        { name: msgs[2].author.username, value: msgs[2].cleanContent, inline: false },
-                        { name: msgs[1].author.username, value: msgs[1].cleanContent, inline: false },
-                        { name: msgs[0].author.username, value: msgs[0].cleanContent, inline: false },
-                        { name: msg.author.username, value: msg.cleanContent, inline: false },
-                        { name: 'Guild', value: `${msg.channel.guild.name}\n(${msg.channel.guild.id})`, inline: true },
-                        { name: 'Channel', value: `#${msg.channel.name}\n(${msg.channel.id})`, inline: true },
-                        { name: 'Msg', value: `(${msg.id})`, inline: true }
-                      ]
-                    }]
-                  }).then(self.deleteWebhook(w.id, w.token))
+                const webhookContent = {
+                  username: msg.author.username,
+                  avatarURL: msg.author.avatarURL,
+                  content: `**I mentioned keyword \`${keywords[i]}\` in** ${msg.channel.mention}!`,
+                  embeds: [{
+                    title: moment(msg.timestamp).format('ddd, Do of MMM @ HH:mm:ss'),
+                    author: {
+                      name: `${msg.author.username}#${msg.author.discriminator} (${msg.author.id})`,
+                      icon_url: msg.author.avatarURL
+                    },
+                    color: 16426522,
+                    fields: [
+                      { name: msgs[2].author.username, value: msgs[2].cleanContent, inline: false },
+                      { name: msgs[1].author.username, value: msgs[1].cleanContent, inline: false },
+                      { name: msgs[0].author.username, value: msgs[0].cleanContent, inline: false },
+                      { name: msg.author.username, value: msg.cleanContent, inline: false },
+                      { name: 'Guild', value: `${msg.channel.guild.name}\n(${msg.channel.guild.id})`, inline: true },
+                      { name: 'Channel', value: `#${msg.channel.name}\n(${msg.channel.id})`, inline: true },
+                      { name: 'Msg', value: `(${msg.id})`, inline: true }
+                    ]
+                  }]
+                }
+                self.getChannelWebhooks(config.notificationChannelID).then(webhooks => {
+                  if (!webhooks || !webhooks.length) {
+                    self.createChannelWebhook(config.notificationChannelID, { name: 'Selfbot' }).then(w => {
+                      self.counts.msgsSent = self.counts.msgsSent + 1
+                      self.executeWebhook(w.id, w.token, webhookContent)
+                    })
+                  } else {
+                    self.counts.msgsSent = self.counts.msgsSent + 1
+                    self.executeWebhook(webhooks[0].id, webhooks[0].token, webhookContent)
+                  }
                 })
               })
             } else { log.err('Invalid config, "notificationChannelID". Must be a channel ID from a server you are in.', 'Mention Stalker'); process.exit() }
