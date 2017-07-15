1418e2863a94750f8194d5a86e9a8d9c7dcbbee3
TheRacingLion <dev@trlcs.com>
2017-07-15 15:23:48 +0100

    Fixing streaming command

--- commands/streaming.js
+++ commands/streaming.js
@@ -3,7 +3,7 @@
 */
 module.exports = (self) => {
   self.registerCommand('streaming', function (msg, args) {
-    this.self.editStatus(this.config.defaultStatus.toLowerCase(), {name: args ? args.join('') : 'nothing', type: 1, url: 'https://www.twitch.tv/twitch'})
+    this.self.editStatus(this.config.defaultStatus.toLowerCase(), {name: args ? args.join(' ') : 'nothing', type: 1, url: 'https://www.twitch.tv/twitch'})
     this.send(msg, '👌')
   })
 }
